ec88f1664944466d16469c1c7666414c556b2b12
Alex <alex@alexseifert.com>
2026-06-01 22:11:54 +0200

    Cleanup

--- Sources/SwiftBlogs/configure.swift
+++ Sources/SwiftBlogs/configure.swift
@@ -8,15 +8,6 @@ import Vapor
 public func configure(_ app: Application) async throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-//    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-//        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-//        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-//        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-//        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-//        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-//        tls: .prefer(try .init(configuration: .clientDefault)))
-//    ), as: .psql)
-    
     app.databases.use(.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
