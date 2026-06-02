25fb634bf223184ea2143b4e722c8785e3c414f3
Alex <alex@alexseifert.com>
2026-06-02 15:12:11 +0200

    Redirect to the login page if the user isn't already logged in

--- Sources/SwiftBlogs/Middleware/SuperAdminMiddleware.swift
+++ Sources/SwiftBlogs/Middleware/SuperAdminMiddleware.swift
@@ -2,7 +2,10 @@ import Vapor
 
 struct SuperAdminMiddleware: AsyncMiddleware {
     func respond(to request: Request, chainingTo next: any AsyncResponder) async throws -> Response {
-        guard let user = request.auth.get(User.self), user.role == .superAdmin else {
+        guard let user = request.auth.get(User.self) else {
+            return request.redirect(to: "/account/login")
+        }
+        guard user.role == .superAdmin else {
             throw Abort(.forbidden, reason: "You do not have permission to access this resource.")
         }
         return try await next.respond(to: request)
