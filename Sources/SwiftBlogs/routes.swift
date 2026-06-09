c4d78285656fed7bc2ddd34c24326352dbbe84a9
Alex Seifert <alex@alexseifert.com>
2026-06-09 15:43:51 +0200

    Remove the root route

--- Sources/SwiftBlogs/routes.swift
+++ Sources/SwiftBlogs/routes.swift
@@ -2,11 +2,11 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws -> View in   
-        let user = req.auth.get(User.self)
-        let username = user?.name ?? nil
-        return try await req.view.render("index", ["title": TitleService.getTitle(), "username": username])
-    }
+//    app.get { req async throws -> View in   
+//        let user = req.auth.get(User.self)
+//        let username = user?.name ?? nil
+//        return try await req.view.render("index", ["title": TitleService.getTitle(), "username": username])
+//    }
 
     try app.register(collection: AccountController())
     try app.register(collection: AdminController())
